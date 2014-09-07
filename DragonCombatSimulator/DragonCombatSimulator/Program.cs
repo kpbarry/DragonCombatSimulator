25e6fc3412aa9cf30bddd1d0c8d630588cc45bd8
kpbarry <kpbarry7@gmail.com>
2014-09-07 17:25:32 -0600

    Made the game more game like.

--- DragonCombatSimulator/DragonCombatSimulator/Program.cs
+++ DragonCombatSimulator/DragonCombatSimulator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace DragonCombatSimulator
     {
         static void Main(string[] args)
         {
+            Console.WindowWidth = 110;
             DragonCombatSimulator();
             Console.ReadKey();
         }
@@ -23,98 +24,96 @@ namespace DragonCombatSimulator
 
             //Rng numbers for everything
             Random rng = new Random();
-            Console.WriteLine(@"______                             _____                 _           _   
-|  _  \                           /  __ \               | |         | |  
-| | | |_ __ __ _  __ _  ___  _ __ | /  \/ ___  _ __ ___ | |__   __ _| |_ 
-| | | | '__/ _` |/ _` |/ _ \| '_ \| |    / _ \| '_ ` _ \| '_ \ / _` | __|
-| |/ /| | | (_| | (_| | (_) | | | | \__/\ (_) | | | | | | |_) | (_| | |_ 
-|___/ |_|  \__,_|\__, |\___/|_| |_|\____/\___/|_| |_| |_|_.__/ \__,_|\__|
-                  __/ |                                                  
-                 |___/                                                   ");
+            Console.WriteLine(@"______                                                   _  ______                                         
+|  _  \                                                 | | |  _  \                                        
+| | | |_ __ __ _  __ _  ___  _ __  ___    __ _ _ __   __| | | | | |_   _ _ __   __ _  ___  ___  _ __  ___  
+| | | | '__/ _` |/ _` |/ _ \| '_ \/ __|  / _` | '_ \ / _` | | | | | | | | '_ \ / _` |/ _ \/ _ \| '_ \/ __| 
+| |/ /| | | (_| | (_| | (_) | | | \__ \ | (_| | | | | (_| | | |/ /| |_| | | | | (_| |  __/ (_) | | | \__ \ 
+|___/ |_|  \__,_|\__, |\___/|_| |_|___/  \__,_|_| |_|\__,_| |___/  \__,_|_| |_|\__, |\___|\___/|_| |_|___/ 
+                  __/ |                                                         __/ |                      
+                 |___/                                                         |___/                        ");
             //Explain the game
-            Console.WriteLine("You are here to slay a dragon. You have 3 choices for options.\n 1. Sword (70% hit, 20-35 damage)\n 2. Magic (100% hit, 10-15 damage)\n 3.Heal (Increases hp 10-20)\nPlease enter the action of your choice.\n");
+            Console.WriteLine("DRAGOOOOONNNNN COMBAAAATTTTTT.\n In tihs game, you are a dungeon trying to kill a dragon. You have 3 options for how to fight the dragon.\n 1. Throw brick (70% hit, 20-35 damage)\n 2. Dust attack (100% hit, 10-15 damage)\n 3. Heal (Repair self for 10-20 HP)\nPlease choose an action.\n");
 
             //Bool for exiting while loop
             bool playing = true;
-            while(playing)
+            while (playing)
             {
                 //Get user input each time
                 string choiceString = Console.ReadLine();
                 int choice = Convert.ToInt32(choiceString);
 
                 //Stop condition for game
-                if (playerHP <= 0 || dragonHP <= 0)
+                if (playerHP > 0 || dragonHP > 0)
+                {
+                    //If user selects attack 1
+                    if (choice == 1)
                     {
-                        if (playerHP <= 0)
+                        attacks++;
+                        //No global RNG values here
+                        int brickHitChance = rng.Next(1, 101);
+                        int brickDmg = rng.Next(20, 36);
+
+                        //30% chance to miss
+                        if (brickHitChance <= 30)
                         {
-                            Console.WriteLine("You dead, son");
-                            playing = false;
+                            Console.WriteLine("You missed! You're a dungeon... Dungeons don't know how to throw bricks!");
                         }
-                        else if (dragonHP <= 0)
+                        else
                         {
-                            Console.WriteLine("You have slain the dragon");
-                            playing = false;
+                            //70% of the time, you hit
+                            dragonHP = dragonHP - brickDmg;
+                            Console.WriteLine("You somehow managed to hit the dragon with a brick, damaging it for " + brickDmg + " damage!");
                         }
                     }
+                    //Dust attack, 100% hit
+                    else if (choice == 2)
+                    {
+                        attacks++;
+                        int dustDmg = rng.Next(10, 16);
+
+                        //You hit the dragon for between 10-15 damage
+                        dragonHP -= dustDmg;
+                        Console.WriteLine("Your dust cloud did " + dustDmg + " damage to the dragon!");
+                    }
+                    //Heal yourself for 10-20 HP
+                    else if (choice == 3)
+                    {
+                        int healAmt = rng.Next(10, 21);
+                        playerHP += healAmt;
+                        Console.WriteLine("You healed for " + healAmt + " HP!");
+                    }
+                    //Incorrect input
                     else
                     {
-                    //If user selects attack 1
-                        if (choice == 1)
-                        {
-                            attacks++;
-                            //No global RNG values here, broseph
-                            int swordHitChance = rng.Next(1, 101);
-                            int swordDmg = rng.Next(20, 36);
-
-                            //30% chance to miss
-                            if (swordHitChance < 30)
-                            {
-                                Console.WriteLine("You missed");
-                            }
-                            else
-                            {
-                                //70% of the time, you hit
-                                dragonHP = dragonHP - swordDmg;
-                                Console.WriteLine("You did " + swordDmg + " damage to the dragon!");
-                            }
-                        }
-                        //Magic attack, 100% hit
-                        else if (choice == 2)
-                        {
-                            attacks++;
-                            int hitChance = rng.Next(1, 101);
-                            int magicDmg = rng.Next(10, 16);
-
-                            //You hit the dragon for between 10-15 damage
-                            dragonHP -= magicDmg;
-                            Console.WriteLine("You did " + magicDmg + " damage to the dragon!");
-                        }
-                        //Heal yourself for 10-20 HP
-                        else if (choice == 3)
-                        {
-                            int healAmt = rng.Next(10, 21);
-                            playerHP += healAmt;
-                            Console.WriteLine("You healed for " + healAmt + " HP!");
-                        }
-                        //Incorrect input
-                        else
-                        {
-                            Console.WriteLine("Please enter either 1, 2 or 3");
-                        }
-                        //Dragon misses 20% of the time
-                        int dragDmg = rng.Next(5, 16);
-                        int dragHitChance = rng.Next(1, 101);
-                        if (dragHitChance < 20)
-                        {
-                            Console.WriteLine("The dragon barely missed you!");
-                        }
-                        //Take damage from dragon
-                        else
-                        {
-                            playerHP -= dragDmg;
-                            Console.WriteLine("The dragon slaps you in the face for " + dragDmg + " damage!");
-                        }
+                        Console.WriteLine("Please enter either 1, 2 or 3");
                     }
+                    //Dragon misses 20% of the time
+                    int dragDmg = rng.Next(5, 16);
+                    int dragHitChance = rng.Next(1, 101);
+                    if (dragHitChance < 20)
+                    {
+                        Console.WriteLine("The dragon barely missed you!");
+                    }
+                    //Take damage from dragon
+                    else
+                    {
+                        playerHP -= dragDmg;
+                        Console.WriteLine("The dragon slaps you in the face for " + dragDmg + " damage!");
+                    }
+                    if (playerHP <= 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("You have lost. Dragons are superior to dungeons.");
+                        playing = false;
+                    }
+                    else if (dragonHP <= 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("DUNGEONS ARE BETTER THAN DRAGONS.");
+                        playing = false;
+                    }
+                }
                 Console.WriteLine("Dragon HP: " + dragonHP);
                 Console.WriteLine("Player HP: " + playerHP);
                 Console.WriteLine("Attacks: " + attacks + "\n");
